90b471e4dd6e375fcf7634baad2a8091ab6bf4b5
rupeshkumar <rupeshkumar9312@gmail.com>
2023-01-10 16:09:51 +0530

    backend-init

--- schemas/index.js
+++ schemas/index.js
@@ -1,1 +1,10 @@
-export const schemaTypes = []
+import abouts from "./abouts";
+import testimonials from "./testimonials";
+import brands from "./brands";
+import contact from "./contact";
+import skills from "./skills";
+import experiences from "./experiences";
+import workExperience from "./workExperience";
+import works from "./works";
+
+export const schemaTypes = [testimonials, abouts, brands, contact, skills, experiences, workExperience, works]
